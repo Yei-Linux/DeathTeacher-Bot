698b501852e96a23582b7dc41a9044940981e893
StartLord11 <jccmunac.97@gmail.com>
2020-07-17 16:41:24 -0500

    [UPDATE] Message Bot

--- controllers/BotController.py
+++ controllers/BotController.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource,Api, reqparse
 from webargs.flaskparser import use_args
 from webargs import fields
-
+from datetime import datetime
 import config.chatterbot as chatterBotConfig
 from helpers.SpeechHelper import SpeechHelper
 
@@ -17,4 +17,4 @@ class BotController(Resource):
     def post(self,request):
         answer = chatterBotConfig.bot.get_response(request['question'])
         self.speechHelper.speakText(answer)
-        return {"answer": str(answer)}
+        return {"type":"message-row you-message","message": str(answer),"date":datetime.now().strftime("%H:%M:%S"),"bot":"bot1"}
