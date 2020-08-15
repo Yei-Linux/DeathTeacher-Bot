ace22b1a7b9d04317b7529cae56484965b2cad5d
7eacemaker <dannynr28@gmail.com>
2020-08-15 01:11:45 -0500

    reestriccion food

--- controllers/WiqipediaController.py
+++ controllers/WiqipediaController.py
@@ -4,6 +4,8 @@ from webargs import fields
 import sys
 sys.path.insert(0, "..")
 from services.FoodService import FoodService
+from services.ThemeService import ThemeService
+
 from datetime import datetime, tzinfo, timedelta
 
 import collections
@@ -28,29 +30,49 @@ class WiqipediaController(Resource):
 
     @use_args(classRequest)
     def post(self, request):
-    	#react
+        #react
         
-        theme_dict= foodHelper().getContentTheme(request['topic'])
-        
-        food_dict = collections.defaultdict(dict)
-        
-        i=0
-        for key in theme_dict:
+        getContentTheme= foodHelper().getContentTheme(request['topic'])
+        theme_dict = getContentTheme[0]
+        theme_url = getContentTheme[1]
+
+        if ThemeService.verifyTheme(theme_url) == True:
+           print("tema buscado")
+           return {"response":theme_dict}
+        else:
             
-            food_dict[str(i)]["id"] = ""
-            food_dict[str(i)]["text"] = "que es" + key +"?"
-            food_dict[str(i)]["search_text"] = ""
-            food_dict[str(i)]["conversation"] =  "training"
-            food_dict[str(i)]["persona"] =  ""
+            print("tema nuevo")
+            food_dict = collections.defaultdict(dict)
+            lista_key = ["url", "content"]
+            lista_value=[theme_url, theme_dict]
+            dictionary_theme = dict(zip(lista_key, lista_value))
+            #prueba = {'url': 'https://es.wikipedia.org/wiki/Matem%C3%A1ticas', 'content': 'hola'}
+            #print(type(dictionary_theme))
+            #print(dictionary_theme)
+            ThemeService.insertTheme(dictionary_theme)
+
+            #ThemeService.insertTheme(theme_dict)
+            i=0
+            for key in theme_dict:
+                food_dict[str(i)]["id"] = ""
+                food_dict[str(i)]["text"] = "que es" + key +"?"
+                food_dict[str(i)]["search_text"] = ""
+                food_dict[str(i)]["conversation"] =  "training"
+                food_dict[str(i)]["persona"] =  ""
+                
+                food_dict[str(i)]["in_response_to"] = theme_dict[key]
+                food_dict[str(i)]["search_in_response_to"] =  theme_dict[key]
+                food_dict[str(i)]["created_at"] = datetime.utcnow().replace(tzinfo=simple_utc()).isoformat()
+                food_dict[str(i)]["tags"] = [theme_dict[key]]
+                i = i+1
             
-            food_dict[str(i)]["in_response_to"] = theme_dict[key]
-            food_dict[str(i)]["search_in_response_to"] =  theme_dict[key]
-            food_dict[str(i)]["created_at"] = datetime.utcnow().replace(tzinfo=simple_utc()).isoformat()
-            food_dict[str(i)]["tags"] = [theme_dict[key]]
-            i = i+1
-        
-        i=0
-        for i in range(0, len(theme_dict)):
-            
-            FoodService.insertFood(food_dict[str(i)])
-        return {"response":theme_dict}
+            i=0
+            for i in range(0, len(theme_dict)):
+                #print(type(food_dict[str(i)]))
+                #print(food_dict[str(i)])
+                FoodService.insertFood(food_dict[str(i)])
+            return {"response":theme_dict}
+
+
+
+       
